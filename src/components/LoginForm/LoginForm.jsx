386db4ba16bc89625b5a5f421e824cb16d1c6757
Ian Lambert <lambe347@umn.edu>
2021-05-25 11:53:37 -0500

    update loginform route to direct user directly to the profile page

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector(store => store.errors);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const login = (event) => {
     event.preventDefault();
@@ -19,9 +21,11 @@ function LoginForm() {
           password: password,
         },
       });
+      history.push('/profile');
     } else {
       dispatch({ type: 'LOGIN_INPUT_ERROR' });
     }
+
   }; // end login
 
   return (
