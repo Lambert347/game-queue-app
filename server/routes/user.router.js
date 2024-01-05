91fc749ab231710a26ca3fafeb2183ade2743bfb
Ian Lambert <lambe347@umn.edu>
2024-01-05 18:07:03 -0500

    fixed logout callback issue

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -42,10 +42,11 @@ router.post('/login', userStrategy.authenticate('local'), (req, res) => {
 });
 
 // clear all server session information about this user
-router.post('/logout', (req, res) => {
-  // Use passport's built-in method to log out the user
-  req.logout();
-  res.sendStatus(200);
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
